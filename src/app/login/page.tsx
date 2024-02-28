9b24a02927a29d6de2f563a1dfec11aa2ec50b22
elad <eladb@radware.com>
2024-02-28 09:59:19 +0200

    add form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,9 @@ import formFactory from "./lib/form";
 
 export default function Login() {
   const form = formFactory.useForm();
-  console.log(form.state);
   return (
     <form.Provider>
-      <form className={"p-3 pt-11 flex  flex-col items-center gap-3"}>
+      <div className={"p-3 pt-11 flex  flex-col items-center gap-3"}>
         <form.Field
           name={"username"}
           validators={{
@@ -14,18 +13,19 @@ export default function Login() {
               if (!value) return "Required";
             },
           }}
-          children={({ state }) => {
+          children={(field) => {
+            console.log(field);
             return (
               <>
                 <input
                   className={"w-min"}
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                  value={state.value}
+                  placeholder="username"
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  value={field.state.value}
                 />
-                {state.error && <div>{state.error}</div>}
+                {field.state.meta.errors && (
+                  <div>{field.state.meta.errors.join(", ")}</div>
+                )}
               </>
             );
           }}
@@ -36,19 +36,10 @@ export default function Login() {
           type="password"
           name="password"
           placeholder="Password"
-          required
         />
         {/*<div>{errorMessage && <p>{errorMessage}</p>}</div>*/}
-        <Submit />
-      </form>
+        <button className={"p-1 border border-amber-500"}>Login</button>
+      </div>
     </form.Provider>
   );
 }
-
-const Submit = () => {
-  return (
-    <button className={"p-1 border border-amber-500"} type="submit">
-      Login
-    </button>
-  );
-};
