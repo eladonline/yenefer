7f5cfeb3bae1ba6d0aad5ae48e630eea5361fd9a
elad <eladb@radware.com>
2024-03-03 11:21:26 +0200

    add jwt

--- src/app/(server)/middlwares/database.ts
+++ src/app/(server)/middlwares/database.ts
@@ -1,19 +1,19 @@
 import { MongoClient } from "mongodb";
-import nextConnect from "";
+import { NextRequest } from "next/server";
 
 const client = new MongoClient(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-async function database(req, res, next) {
+type DBReq = {
+  dbClient: MongoClient;
+  db:
+};
+
+async function database(req: NextRequest) {
   req.dbClient = client;
   req.db = client.db("MCT");
-  return next();
 }
 
-const middleware = nextConnect();
-
-middleware.use(database);
-
-export default middleware;
+export default database;
