17327347a50087cfe8f0c136aaad9931c6124491
elad <eladb@radware.com>
2024-03-29 13:53:39 +0300

    create modal context

--- src/app/(pages)/(home)/products/lib/Form.tsx
+++ src/app/(pages)/(home)/products/lib/Form.tsx
@@ -4,6 +4,7 @@ import QueryClientProvider from "@/utils/Providers/QueryClientProvider";
 import useProduct from "@/app/(pages)/(home)/products/lib/useProduct";
 import { ProductType } from "@/types/apis/usersData";
 import Menu from "@/app/(pages)/(home)/products/lib/Menu";
+import { Modal } from "antd/lib";
 
 type FormProps = {
   data: ProductType[];
@@ -11,6 +12,7 @@ type FormProps = {
 
 const Form: React.FC<FormProps> = ({ data }) => {
   const { products, isLoading } = useProduct(data);
+  const [modal, contextHolder] = Modal.useModal();
 
   const productsEl: ReactElement<ProductType>[] | undefined = products?.map(
     ({ name }) => {
@@ -19,11 +21,14 @@ const Form: React.FC<FormProps> = ({ data }) => {
   );
 
   return (
-    <div className={"p-5 bg-white rounded"}>
-      <Menu />
-      <QueryClientProvider>
-        <ul className={"flex-col flex gap-3"}>{productsEl}</ul>
-      </QueryClientProvider>
+    <div className={"grid grid-cols-1 gap-[20px]"}>
+      <div>{contextHolder}</div>
+      <Menu modalApi={modal} />
+      <div className={"p-5 bg-white rounded "}>
+        <QueryClientProvider>
+          <ul className={"flex-col flex gap-3"}>{productsEl}</ul>
+        </QueryClientProvider>
+      </div>
     </div>
   );
 };
--- src/app/(pages)/(home)/products/lib/Menu.tsx
+++ src/app/(pages)/(home)/products/lib/Menu.tsx
@@ -2,11 +2,25 @@ import React from "react";
 import { Button, Tooltip } from "antd/lib";
 import { PlusOutlined } from "@ant-design/icons";
 
-const Menu = () => {
+const Menu = ({ modalApi }) => {
   return (
-    <div className={"p-2 bg-stone-700 rounded-2xl"}>
+    <div className={"p-2 bg-stone-700 rounded-2xl sticky top-0 z-10"}>
       <Tooltip title={"Add product"}>
         <Button
+          onClick={() => {
+            modalApi.confirm({
+              title: "confirm title",
+              content: "confirm content",
+              onOk: () => {
+                console.log("asdasdsad");
+              },
+            });
+
+            modalApi.info({
+              title: "info title",
+              content: "info content",
+            });
+          }}
           className={
             "ovd ovd2 [&.ant-btn]:bg-green-400 hover:[&.ovd.ovd2.ant-btn]:bg-green-300 active:[&.ovd.ovd2.ant-btn]:bg-green-600"
           }
