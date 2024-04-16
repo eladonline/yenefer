a9056f1f13e4f4461933e6311cf1bf1144f3667e
elad <eladb@radware.com>
2024-04-16 20:53:41 +0300

    move validation from form to file

--- src/app/(pages)/(home)/my-products/lib/ProductTermsBar.tsx
+++ src/app/(pages)/(home)/my-products/lib/ProductTermsBar.tsx
@@ -10,6 +10,7 @@ import _get from "lodash/get";
 import dayjs from "dayjs";
 import { ProductFormType } from "@/types/apis/usersData";
 import Decimal from "decimal.js";
+import { validateMinPrice } from "@/app/(pages)/(home)/my-products/lib/validations";
 
 const options = [
   { label: "%", value: "percentage" },
@@ -73,77 +74,7 @@ const ProductTermsBar: FC<{ disabled: boolean }> = ({ disabled }) => {
             disabled={disabled}
             rules={{
               required: "Field Required",
-              validate: (
-                minPrice: number,
-                {
-                  price,
-                  terms: {
-                    discount_each_buyer: {
-                      value: discountEachBuyerValue,
-                      unit: discountEachBuyerUnit,
-                    },
-                    quantity,
-                  },
-                }: ProductFormType,
-              ) => {
-                if (
-                  !price ||
-                  !discountEachBuyerValue ||
-                  !discountEachBuyerUnit ||
-                  !quantity
-                )
-                  return;
-                const priceDecimal = new Decimal(price);
-                const minPriceDecimal = new Decimal(minPrice);
-
-                if (discountEachBuyerUnit !== "percentage") {
-                  const discountDecimal = new Decimal(discountEachBuyerValue);
-
-                  if (
-                    priceDecimal
-                      .minus(discountDecimal)
-                      .lessThan(minPriceDecimal)
-                  )
-                    return `Price - Discount is lower/equal to ${priceDecimal.minus(discountDecimal)}`;
-
-                  if (!minPriceDecimal.modulo(discountDecimal).isZero())
-                    return `Price with Discount will never be ${minPrice}`;
-
-                  if (priceDecimal.lessThanOrEqualTo(minPriceDecimal))
-                    return `Minimum price should be lower then ${price}`;
-
-                  if (
-                    priceDecimal
-                      .minus(discountDecimal.times(quantity))
-                      .gt(minPriceDecimal)
-                  )
-                    return `Price with Discount and quantity will never be ${minPrice}`;
-                }
-
-                if (discountEachBuyerUnit === "percentage") {
-                  const discountDecimal = new Decimal(discountEachBuyerValue);
-
-                  const discountAsInteger = new Decimal(
-                    discountDecimal.dividedBy(100).times(priceDecimal),
-                  );
-
-                  const priceAfterDiscount =
-                    priceDecimal.minus(discountAsInteger);
-
-                  if (priceAfterDiscount.lt(minPriceDecimal))
-                    return `Price - Discount should be higher/equal ${minPrice}`;
-
-                  if (!minPriceDecimal.modulo(discountAsInteger).isZero())
-                    return `Price - Discount * Buyers will never equal to ${minPrice}`;
-
-                  if (
-                    priceDecimal
-                      .minus(discountAsInteger.times(quantity))
-                      .gt(minPriceDecimal)
-                  )
-                    return `Price - Discount * Quantity will never be ${minPrice}`;
-                }
-              },
+              validate: validateMinPrice,
             }}
             name={"terms.min_price"}
             min={0}
