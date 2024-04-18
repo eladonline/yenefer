0cf20290a2ca14a2ccd38b5874bcf864c3fdae9a
elad <eladb@radware.com>
2024-04-18 08:48:02 +0300

    create cloudinary service

--- src/app/(server)/api/route.ts
+++ src/app/(server)/api/route.ts
@@ -1,31 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
-import { Readable } from "stream";
-
-const { CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY } =
-  process.env;
-
-cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
-});
+import cloudinaryService from "@/app/services/cloudinary";
 
 export async function test(req: NextRequest) {
   try {
-    const buffer = fs.readFileSync("./test.png");
-    const stream = Readable.from(buffer);
-
-    let cld_upload_stream = cloudinary.uploader.upload_stream(
-      {
-        folder: "foo",
-      },
-      function (error, result) {
-        console.log(error, result);
-      },
-    );
-    stream.pipe(cld_upload_stream);
+    const res = await cloudinaryService.api.upload("./test.png", {
+      folder: "foo",
+    });
+    console.log(res);
   } catch (err: any) {
     console.log(err.stack);
   }
---
+++ src/app/services/cloudinary.ts
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from "cloudinary";
+
+const { CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY } =
+  process.env;
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+class Cloudinary {
+  get api() {
+    return cloudinary.uploader;
+  }
+}
+
+const cloudinaryService = new Cloudinary();
+
+export default cloudinaryService;
